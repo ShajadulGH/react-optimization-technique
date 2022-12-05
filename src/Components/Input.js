5453c2634d5703edb99f6ddbfddd6326cabfb7fa
Shajadul <shajadulrahe@gmail.com>
2022-12-05 07:38:06 +0600

    Feat: Use class based component

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,6 +1,13 @@
-import React from "react";
-const Input = (props) => {
-  console.log("Input Running");
-  return <input onChange={props.onChange} type="number" />;
-};
+// import React from "react";
+// const Input = (props) => {
+//   console.log("Input Running");
+//   return <input onChange={props.onChange} type="number" />;
+// };
+// export default Input;
+import { Component } from "react";
+class Input extends Component {
+  render() {
+    return <input onChange={this.props.onChange} type="number" />;
+  }
+}
 export default Input;
